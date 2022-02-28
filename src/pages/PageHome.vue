85d0846d45b87acbd7d9fc24cdbde3020e9cd08c
Ajuka David <ajuka.zephiniah@gmail.com>
2022-02-28 23:07:06 +0100

    added delete qweet and date method

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -21,6 +21,7 @@
       </div>
       <div class="col col-shrink">
         <q-btn
+        @click="addNewQweet"
          color="primary"
          label="Qweet"
          rounded
@@ -38,9 +39,12 @@
  class="divider"
  />
 
- <q-list
- >
-      <q-item class="q-py-md">
+ <q-list separator>
+      <q-item
+      v-for="qweet in qweets"
+      :key="qweet.date"
+       class="q-py-md"
+       >
         <q-item-section avatar top>
           <q-avatar>
             <img src="https://s.gravatar.com/avatar/d44fdfe34fc3905a5fb25613aa07c4a2?s=80">
@@ -57,9 +61,8 @@
             </span>
           </q-item-label>
           <q-item-label class="qweet-content text-body1">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. At maxime quis corporis provident laborum veritatis! Quia ex voluptatem, id provident voluptas nemo
-             accusantium, nihil suscipit corrupti laboriosam expedita a maiores.
-          </q-item-label>
+           {{qweet.content}}
+           </q-item-label>
           <div class="row qweet-icons justify-between q-mt-sm">
             <q-btn
             color="grey"
@@ -88,28 +91,72 @@
             flat
             size="sm"
             round
+            @click="deleteQweet(qweet)"
              />
           </div>
         </q-item-section>
 
         <q-item-section side top>
-          1 min ago
+         {{relativeDate(qweet.date)}} ago
         </q-item-section>
       </q-item>
-
 
     </q-list>
   </q-page>
 </template>
 
 <script>
+import { formatDistance } from 'date-fns'
+
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageHome',
   data(){
     return {
-      newQweetContent: ''
+      newQweetContent: '',
+      qweets: [
+        {
+          content: `
+          Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Quidem expedita ipsum
+          fugiat at quas dicta repellat veniam
+          perspiciatis porro iure. Voluptate
+          pariatur dicta nobis! Deleniti necessitatibus
+           blanditiis ex recusandae atque?
+          `,
+          date: 1646082317300
+        },
+         {
+          content: `
+          Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Quidem expedita ipsum
+          fugiat at quas dicta repellat veniam
+          perspiciatis porro iure. Voluptate
+          pariatur dicta nobis! Deleniti necessitatibus
+           blanditiis ex recusandae atque?
+          `,
+          date: 1646082356035
+        }
+      ]
+    }
+  },
+  methods: {
+    relativeDate(value) {
+      return formatDistance(value, new Date())
+    },
+    addNewQweet(){
+     let newQweet = {
+       content: this.newQweetContent,
+       date: Date.now()
+     }
+     this.qweets.unshift(newQweet)
+     this.newQweetContent = ''
+    },
+    deleteQweet(qweet){
+      let dateToDelete = qweet.date
+      let index = this.qweets.findIndex(qweet => qweet.date === dateToDelete)
+      this.qweets.splice(index, 1)
     }
   }
 })
